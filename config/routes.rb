54c51254b87a5629609bc78e8aebb113f16f3e54
Kyle Demeule <kyle.demeule@goclio.com>
2013-08-29 18:22:47 -0700

    Adding basic board view/controller/model with new index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Lerdehrberd::Application.routes.draw do
+  get "boards/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Lerdehrberd::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'boards#index'
 
   # See how all your routes lay out with "rake routes"
 
