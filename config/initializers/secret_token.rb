3bc7d415eb5dc91807d85648a8fbf80b95f01804
Kyle Demeule <kyle.demeule@goclio.com>
2013-08-29 17:44:54 -0700

    Adding blank initial rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lerdehrberd::Application.config.secret_token = '2913feeb3115f4c3ca18b4c75da7b51540eb22f4b5de83cdd3f2330d2813e36d83811d4b24aae0860c4f5723cf477ef2a0ea239b6ed8303f325973f1b229e25f'
